82960e54446c30abfbc7e9653a7ff70c5a6e2f74
azkawell <64977910+azkawell@users.noreply.github.com>
2023-06-23 00:47:04 +0300

    initial commit

---
+++ build/precache-manifest.d79826139cc4660031d94f864dd8ef3b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed4c727b5e0dd7aa3ea1eecb3120121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7e7de8853c0d63ad3b1",
+    "url": "/static/js/0.a6ee5393.chunk.js"
+  },
+  {
+    "revision": "0821c80bc14785475faf",
+    "url": "/static/js/10.4ec639b8.chunk.js"
+  },
+  {
+    "revision": "ce8fc368a560bd323905",
+    "url": "/static/js/11.410239f6.chunk.js"
+  },
+  {
+    "revision": "dfaebdcdf9657845c99a",
+    "url": "/static/js/3.dac5dcdf.chunk.js"
+  },
+  {
+    "revision": "92426c7afbed18191e6269947752cdd5",
+    "url": "/static/js/3.dac5dcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef3e0c4756d093fcee8",
+    "url": "/static/js/4.77bfe80b.chunk.js"
+  },
+  {
+    "revision": "8e7b2fc0d4b8f8a476e2",
+    "url": "/static/js/5.5d61d7a3.chunk.js"
+  },
+  {
+    "revision": "3d493347cbf0ebaf193563102afcd817",
+    "url": "/static/js/5.5d61d7a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896bf9337bd246a78fa8",
+    "url": "/static/js/6.f11787e7.chunk.js"
+  },
+  {
+    "revision": "63d3855896d6662f2d60",
+    "url": "/static/js/7.a1085d7b.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/7.a1085d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec3fd7d98fb48ae78ab",
+    "url": "/static/js/8.b0ebe753.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.b0ebe753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe3e12edded03493d47",
+    "url": "/static/js/9.d61cca7d.chunk.js"
+  },
+  {
+    "revision": "c4eba43a0b51076d8232",
+    "url": "/static/js/main.0ab238b3.chunk.js"
+  },
+  {
+    "revision": "4d0c7a955801cc474556",
+    "url": "/static/js/runtime-main.1f816db8.js"
+  },
+  {
+    "revision": "4c5094d5678db7d668ca893a84a93e1c",
+    "url": "/static/media/logoletras.4c5094d5.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "/static/media/wallet-connect.d1f73612.svg"
+  }
+]);
---
+++ build/static/js/11.410239f6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpyam=this.webpackJsonpyam||[]).push([[11],{843:function(p,s){}}]);
+//# sourceMappingURL=11.410239f6.chunk.js.map
